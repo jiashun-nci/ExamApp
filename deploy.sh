4628dc2a288a7ffc7105bf2b1437b3dce047e41b
jiashun-nci <73401615+jiashun-nci@users.noreply.github.com>
2023-02-14 21:30:43 +0000

    added continuous delivery

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# update system and install nodejs and npm
+sudo apt update && sudo apt install nodejs npm
+# install pm2
+sudo npm install -g pm2
+# stop all instance of running app
+pm2 stop ExamApp
+# change to working folder
+cd ExamApp
+# install dependencies
+sudo npm install
+# start application
+pm2 start ./bin/www -name ExamApp --env=production
